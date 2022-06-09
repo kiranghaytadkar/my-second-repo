20ebaa3ffd0e69a4ecb12a4a4edb8ec6c3c274de
kiran <kiran@DESKTOP-KSV9KVJ>
2022-06-09 20:14:55 +0530

    xyz

---
+++ src/DEMO/test1.java
@@ -0,0 +1,13 @@
+package DEMO;
+
+public class test1 {
+	
+	public static void main(String args[]) {
+		System.out.println("kiran");
+		int a= kiran(3,5);
+	}
+	public static int kiran(int a,int b) {
+		System.out.println(a+b);
+		return a+b;
+	}
+}
---
+++ src/main1/jawa.java
@@ -0,0 +1,9 @@
+package main1;
+
+public class jawa {
+	public static void main(String[]args) {
+
+			
+				System.out.println("kiran");
+}
+}
